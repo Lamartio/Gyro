462142a5019541bde29bbe62680666a162b16cb4
DannyLamarti <lamarti.danny@gmail.com>
2019-07-03 21:07:49 +0200

    Improve publishing script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-sh publish-lib.sh $@ && sleep 10 && sh publish-livedata.sh $@ && sh publish-rxjava2.sh $@
+sh publish-lib.sh $@
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+   echo .
+   sleep 1
+done
+
+sh publish-livedata.sh $@
+sh publish-rxjava2.sh $@
